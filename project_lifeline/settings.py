499be9012576ed30a1b9edadc14613948f032b1f
Victor <victor.as.nascimento@gmail.com>
2021-03-14 17:07:28 -0300

    initial commit

--- project_lifeline/settings.py
+++ project_lifeline/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 
