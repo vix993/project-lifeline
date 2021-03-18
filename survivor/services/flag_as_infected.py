864d83aee50de63569c5e68d8868722c8f134b4a
Victor <victor.as.nascimento@gmail.com>
2021-03-17 22:01:34 -0300

    oops forgot his

--- survivor/services/flag_as_infected.py
+++ survivor/services/flag_as_infected.py
@@ -1,12 +1,14 @@
 from survivor.models import Survivor, FlagAsInfected
 
+from rest_framework.serializers import ValidationError
+
 def do_flag_as_infected(request):
     qs = FlagAsInfected.objects.\
-        filter(flager_pk__exact=self.request.data['flager_pk']).\
-        filter(flaged_pk__exact=self.request.data['flaged_pk'])
+        filter(flager_pk__exact=request.data['flager_pk']).\
+        filter(flaged_pk__exact=request.data['flaged_pk'])
     to_increment = int(request.data['flaged_pk']) - 1
     if qs:
-        raise serializers.ValidationError("We have already received your flag")
+        raise ValidationError("We have already received your flag")
     survivor_obj = Survivor.objects.all()[to_increment]
     survivor_obj.infection_marks += 1
     if survivor_obj.infection_marks > 4:
