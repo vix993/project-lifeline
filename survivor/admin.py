682f8095b7efab1824f535d831f0c10467b7b5c2
Victor <victor.as.nascimento@gmail.com>
2021-03-19 09:29:20 -0300

    adding models to admin because why not

--- survivor/admin.py
+++ survivor/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 
+from .models import Survivor, FlagAsInfected, Reports, TradeItem
+
+admin.site.register(Survivor)
+admin.site.register(FlagAsInfected)
+admin.site.register(TradeItem)
+admin.site.register(Reports)
+
